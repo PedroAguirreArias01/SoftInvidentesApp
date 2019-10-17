c8cbe2a6aca32d5068edaac0629ecc9e08fb579e
PedroAguirreArias01 <20175260+PedroAguirreArias01@users.noreply.github.com>
2019-10-16 22:36:52 -0500

    modulo de usuarios

--- invidentes-app/src/app/app.module.ts
+++ invidentes-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
---
+++ invidentes-app/src/app/usuarios/usuario.ts
@@ -0,0 +1,9 @@
+export class Cliente {
+    id: number;
+    nombre: string;
+    apellido: string;
+    createAt: string;
+    email: string;
+    usuario: string;
+    password: string;
+}
