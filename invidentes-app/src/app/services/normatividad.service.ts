57c72bec5564349b52ac497c6274b3120f3c3161
Pedro Aguirre Arias <pedro_1995pe@hotmail.com>
2019-12-07 11:22:58 -0500

    Modulo de normatividad

--- invidentes-app/src/app/services/normatividad.service.ts
+++ invidentes-app/src/app/services/normatividad.service.ts
@@ -1,41 +1,35 @@
 import { Injectable, Injector } from '@angular/core';
-import { ColaboradorDTO } from '../dto/colaborador.dto';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from "@angular/common/http";
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 /**
- * @description clase que se encarga de gestionar los  servicios de colaborador
+ * @description clase que se encarga de gestionar los  servicios de normatividad
  * @author Pedro Aguirre Arias
  */
 @Injectable({
   providedIn: 'root'
 })
-export class ColaboradorService {
-
+export class NormatividadService {
   /**
    * Url que define la localizacion de los recursos 
    */
-  private urlEndPoint: string = 'http://localhost:8080/api/colaboradores/';
+  private urlEndPoint: string = 'http://localhost:8080/normatividad/';
 
-   /**
-   * @description contructor de la clase
-   */
-  constructor(injector: Injector, private http : HttpClient) { }  
+  constructor(injector: Injector, private http : HttpClient) { }
 
-
+  
   /**
-   * @description metodo encargado de obtener todos los colaboradores que se encuentran en la base de datos
+   * @description metodo encargado de obtener toda la normatividad que se encuentra en la base de datos
    * @author Pedro Aguirre Arias <pedro.aguirre@uptc.edu.co>
    */
-  getColaboradores(): Observable<any> {
+  getNormatividad(): Observable<any> {
     return this.http.get(this.urlEndPoint);
   }
 
   /**
-   * @description Metodo encargado de crear el colaborador
+   * @description Metodo encargado de crear la normatividad
    * @param colaboradorDTO 
    */
-  crearColaborador(colaboradorDTO: ColaboradorDTO): Observable<any> {
+  crear(colaboradorDTO: ColaboradorDTO): Observable<any> {
     return this.http.post(this.urlEndPoint, colaboradorDTO);
   }
 
